6af11eeb8f84c393cc385a3c359dd7aca8a20ad0
felix1251 <domhack123@gmail.com>
2022-11-06 20:02:51 +0800

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
-    
   devise :database_authenticatable, :registerable, :trackable,
           :recoverable, :rememberable, :validatable, :confirmable,
           :omniauthable, omniauth_providers: %i[google_oauth2] 
 
   include  DeviseTokenAuth::Concerns::User 
-
-  has_many :profile_images, :dependent => :destroy
 
   validates :email,
             format: { with: URI::MailTo::EMAIL_REGEXP },
@@ -16,7 +13,8 @@ class User < ApplicationRecord
   private
 
   def self.signin_or_create_from_google(data)
-    where(provider: data["iss"], uid: data["sub"], email: data["email"]).first_or_create do |user|
+    where(uid: data["sub"], email: data["email"]).first_or_create do |user|
+      user.provider = "google_oauth2"
       user.password = Devise.friendly_token[0, 20]
       user.skip_confirmation! 
     end
