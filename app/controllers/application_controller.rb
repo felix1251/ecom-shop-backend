56b773fc98b9e7a9eea5027152721e539da433e8
felix1251 <domhack123@gmail.com>
2022-11-06 19:58:52 +0800

    get current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,4 @@ class ApplicationController < ActionController::API
       User.select("id, email, name, provider").find_by(id: session["warden.user.user.key"][0][0]) 
     end
   end
-
-  def admin_only
-    unless current_user.is_admin?
-      render json: { error: "forbidden" } , status: :forbidden
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,12 @@ end
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks, :sessions, :registration, :password, :validations, :token_validations]
-  
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       #auth controllers
-      post 'auth/google/callback', to: 'social_auth#authenticate_by_google'
-      post 'auth/email', to: 'social_auth#authenticate_by_email'
-      post 'auth/signup_by_email', to: 'social_auth#sign_up_by_email'
-      get 'current_user/me', controller: :infos, action: :me
+      post 'google_auth/callback', to: 'social_auth#authenticate_by_google'
+      get 'me', controller: :infos, action: :me
       delete "logout", to: 'social_auth#log_out'
       #crud controllers
       resources :users
